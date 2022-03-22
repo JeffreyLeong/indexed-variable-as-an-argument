f4b6ae0f84be40d3cf21bea0130351bd4572db02
dollarortwo <jleong001@gmail.com>
2022-03-21 21:04:17 -0700

    fixed output: vacation[number-1]

--- Indexed-variables-arrays.cpp
+++ Indexed-variables-arrays.cpp
@@ -10,15 +10,20 @@ int adjust_days(int old_days); // Returns old_days plus 5
 int main()
 {
     int vacation[NUMBER_OF_EMPLOYEES], number;
-    cout << "Enter allowed vacation days for employees 1 through " << NUMBER_OF_EMPLOYEES << ".\n";
+    // user input
+    cout << "Enter allowed vacation days for employees 1 through " << NUMBER_OF_EMPLOYEES << ":\n";
+
+    // loop and store input
     for(number = 1; number <= NUMBER_OF_EMPLOYEES; number++)
         cin >> vacation[number - 1];
     for(number = 0; number < NUMBER_OF_EMPLOYEES; number++)
         vacation[number] = adjust_days(vacation[number]);
+    
+    // output
     cout << "The revised number of vacation days are:\n";
     for(number = 1; number <= NUMBER_OF_EMPLOYEES; number++)
-        cout << "Employee number " << number << " vacation days = " << vacation[number] << endl;
- return 0;
+        cout << "Employee number " << number << " vacation days = " << vacation[number-1] << endl;
+    return 0;
 }
 
 int adjust_days(int old_days)
